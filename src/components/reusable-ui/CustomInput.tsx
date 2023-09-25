483c9a991eedf7e822fcf6d7eea50258429e8fa2
Maxime Danic <maxime.danic@gmail.com>
2023-09-25 21:49:58 +0300

    ✨F01: CUSTOMINPUT > Add different version style

--- src/components/reusable-ui/CustomInput.tsx
+++ src/components/reusable-ui/CustomInput.tsx
@@ -1,21 +1,51 @@
+import { ChangeEvent } from 'react';
+
 interface CustomInputProps {
   label: string;
   icon: React.ReactNode;
   placeholder: string;
   id: string;
+  version: 'default' | 'variant1' | 'variant2';
+  type: string;
+  value: string;
+  name: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-function CustomInput({ label, icon = '', placeholder, id }: CustomInputProps) {
+function CustomInput({
+  label,
+  icon,
+  placeholder,
+  id,
+  version = 'default',
+  type,
+  value,
+  name,
+  onChange,
+}: CustomInputProps) {
+  const versionsStyles: Record<string, string> = {
+    default:
+      'transition border-border-pri border rounded-lg w-full h-12 pl-11 placeholder:truncate placeholder:text-dark-grey/50 hover:border-purple-pri hover:shadow-purple-pri/20 hover:shadow-lg focus:shadow-purple-pri/20 focus:shadow-lg focus:outline-purple-pri caret-purple-pri',
+    variant1:
+      'border-border-pri border rounded-lg w-full h-12 pl-11 placeholder:text-dark-grey/50 hover:border-red-pri hover:shadow-purple-pri/20 hover:shadow-lg transition',
+
+    variant2: '...',
+  };
+  const style = versionsStyles[version] || versionsStyles.default;
+
   return (
     <label htmlFor={id} className="flex flex-col gap-2 w-full">
-      <span className="text-xs text-dark-grey font-light">{label}</span>
+      <span className="text-xs text-dark-grey font-light ">{label}</span>
 
       <div className="relative">
         <input
-          type="text"
+          type={type}
           id={id}
           placeholder={placeholder}
-          className="gap-4 border-border-pri border rounded-lg w-full h-12 pl-11 placeholder:text-dark-grey/50 hover:border-purple-pri hover:shadow-purple-pri/20 hover:shadow-lg transition"
+          className={style}
+          value={value}
+          onChange={onChange}
+          name={name}
         />
         {icon && (
           <div className="absolute left-4 bottom-1/2 text-light-grey text-base translate-y-[55%]">
