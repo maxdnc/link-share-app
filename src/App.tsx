f679c07439252995d8754285608c882ec3c0f4d2
Maxime Danic <maxime.danic@gmail.com>
2023-10-09 12:52:48 +0200

    ✨ F02: APP > Add new routes: links details preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 // pages
 import Login from './components/pages/authentification/Login';
 import SignIn from './components/pages/authentification/SignIn';
-import Home from './components/pages/home/Home';
+
+import Links from './components/pages/home/Links';
+import Details from './components/pages/home/Details';
+import MainLayout from './components/pages/home/MainLayout';
+import Preview from './components/pages/preview/Preview';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Navigate to="/links" />} />
+          <Route path="/links" index element={<Links />} />
+          <Route path="/details" element={<Details />} />
+        </Route>
+        <Route path="/preview" element={<Preview />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signin" element={<SignIn />} />
       </Routes>
